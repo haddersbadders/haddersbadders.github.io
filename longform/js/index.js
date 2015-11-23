8966656eef4f46aa16e7fc57095ba8c7bf8e19da
haddersbadders <hadders@dyn137087.shef.ac.uk>
2015-11-23 15:41:57 +0000

    Added Scroll magic

--- longform/js/index.js
+++ longform/js/index.js
@@ -1,1 +1,14 @@
 new WOW().init();
+
+var controller = new ScrollMagic.Controller();
+
+	// build scene
+	var scene = new ScrollMagic.Scene({triggerElement: "#flowers", duration: 500})
+					.addTo(controller)
+
+					.on("enter", function () {
+            document.getElementById('mountain').play();
+					})
+          .on("leave", function () {
+            $('#mountain').trigger("pause");
+          });
