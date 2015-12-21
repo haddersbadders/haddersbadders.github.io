c7d36c708218f2a64e738b686d54c1b9af770c6f
Hadrian Cawthorne <haddersbadders@gmail.com>
2015-12-21 10:19:09 +0000

    Update index.js

--- longform/js/index.js
+++ longform/js/index.js
@@ -85,7 +85,7 @@ function onPlayerReady(event) {
 var done = false;
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 600000);
+    setTimeout(stopVideo, 6000);
     done = true;
   }
 }
